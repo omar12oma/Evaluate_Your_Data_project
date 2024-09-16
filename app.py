f14d6ef5b56d4443fee9958fd6d4446c7fa35702
Abdullah <aasbobodd19@gmail.com>
2024-09-16 17:50:08 +0300

    working version

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import streamlit as st
 from evaluation import evaluate
+import pandas as pd
+
 st.title("Score Project")
 csvfile=st.file_uploader("Upload your CSV file", type=["csv"])
 
 if csvfile is not None:
     st.write("Your CSV file has been uploaded.")
-    st.write(csvfile)
-    score=evaluate(csvfile)
-    st.write(score)
+    # df = pd.read_csv(csvfile)
+    # st.write(df)
+    # st.write(csvfile)
+    
+
+    score=evaluate(csvfile)*10
+    # st.markdown("---")  # Separator for better visual separation
+    # st.header("Your Score")
+    
+    # # Option 1: Using a metric card
+    # st.metric(label="", value=str(score))
+        # Option 2: Using a progress bar (if score is a percentage)
+    # if 0 <= score <= 10:
+    #     st.progress(score)
+    #     st.write(f"Your score is: {score}%") 
+    
+    # Option 3: Using a colored box
+    if score >= 80:
+        st.success(f"Your score is: **{score}**. Excellent!")
+    elif score >= 60:
+        st.info(f"Your score is: **{score}**. Good!")
+    else:
+        st.warning(f"Your score is: **{score}**. Needs improvement.")
 else:
-    st.write("Please upload your CSV file.")    
+    st.write("Please upload your CSV file.")
--- evaluation.py
+++ evaluation.py
@@ -1,2 +1,37 @@
+import pandas as pd
+import numpy as np
 def evaluate(csvfile):
-    return 10
+    df = pd.read_csv(csvfile)
+    duplicated = duplicated_score(df)
+    columns_type = columns_type_score(df)
+    isna = isna_score(df)
+    score = 0.2*duplicated + 0.4*columns_type + 0.4*isna
+    return score*10
+    
+def duplicated_score(df):
+   Number_duplicat = df.duplicated().sum()
+   score = 1-Number_duplicat*2/(df.shape[0]-Number_duplicat)
+   adj_score = np.square(score)
+   return adj_score
+def columns_type_score(df):
+    object_columns = df.columns
+    count = 0
+    for col in object_columns:
+        num_type = df[col].dropna().apply(type).nunique()
+        if num_type != 1:
+            count += 1
+        score = 1-count/len(object_columns)
+        adj_score = np.square(score)
+    return adj_score
+def isna_score(df):
+    col_na = df.isna().any().sum()
+    num_col =df.shape[1]
+    score = 1 - col_na/num_col
+    adj_score = np.square(score)
+    return adj_score
+def Evaluate_Your_Data(df):
+    duplicated = duplicated_score(df)
+    columns_type = columns_type_score(df)
+    isna = isna_score(df)
+    score = 0.2*duplicated + 0.4*columns_type + 0.4*isna
+    return score*10
